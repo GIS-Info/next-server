9e3593489addb19c782ca78c8e7fd9d86112b80b
Xuanh.W <Xuanhao.W.coding@outlook.com>
2022-04-02 12:20:58 +0800

    :bug:: use username instead of id to filter user

--- app/apis/user_list/views.py
+++ app/apis/user_list/views.py
@@ -1,10 +1,10 @@
-from rest_framework.viewsets import ModelViewSet
 from django.contrib.auth.models import User
 from .serializer import UserListSerializer
 from ...viewsets import ModelViewSetWithCustomResponse
+from app.response import SuccessResponse
 
 
 class UserListView(ModelViewSetWithCustomResponse):
     queryset = User.objects.all()
     serializer_class = UserListSerializer
-
+    lookup_field = 'username'
