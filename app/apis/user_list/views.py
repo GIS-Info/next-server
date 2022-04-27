3c60dc9b11ea5dcb07d050ca66101e0e529be34b
Xuanh.W <Xuanhao.W.coding@outlook.com>
2022-04-27 12:06:48 +0800

    refactor: fix merge

--- app/apis/user_list/views.py
+++ app/apis/user_list/views.py
@@ -1,23 +1,12 @@
-from rest_framework.viewsets import ModelViewSet
-from django.contrib.auth.models import User
-from .serializer import UserListSerializer
-from ...viewsets import ModelViewSetWithCustomResponse
-
-
-class UserListView(ModelViewSetWithCustomResponse):
-    queryset = User.objects.all()
-    serializer_class = UserListSerializer
-
-
 from django.contrib.auth.models import User
 from rest_framework.renderers import BrowsableAPIRenderer
 from rest_framework.viewsets import ModelViewSet
 from .serializer import UserListSerializer
-from ...renders import SuccessAPIRenderer
+from ...renders import APIRenderer
 
 
 class UserListView(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserListSerializer
     lookup_field = 'username'
-    renderer_classes = [SuccessAPIRenderer, BrowsableAPIRenderer]
+    renderer_classes = [APIRenderer, BrowsableAPIRenderer]
