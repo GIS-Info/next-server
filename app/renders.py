4762f231491f3bfbb3a8dbca79fa93a24c00521e
Xuanh.W <Xuanhao.W.coding@outlook.com>
2022-04-27 12:07:40 +0800

    fix: return code 1 when user not found

--- app/renders.py
+++ app/renders.py
@@ -1,13 +1,19 @@
 from rest_framework.renderers import JSONRenderer
-
-
-class SuccessAPIRenderer(JSONRenderer):
+class APIRenderer(JSONRenderer):
 
     def render(self, data, accepted_media_type=None, renderer_context=None):
-        response = {
-            'code': 0,
-            'message': 'success',
-            'data': data
-        }
+        error_msg = data.get('detail', None)
+        if error_msg:
+            response = {
+                'code': 1,
+                'message': error_msg.code,
+                'data': ""
+            }
+        else:
+            response = {
+                'code': 0,
+                'message': 'success',
+                'data': data
+            }
 
-        return super(SuccessAPIRenderer, self).render(response, accepted_media_type, renderer_context)
+        return super(APIRenderer, self).render(response, accepted_media_type, renderer_context)
