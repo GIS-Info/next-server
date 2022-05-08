360d3907c2a16f962d60e20f039e57a88dbc5982
Xuanh.W <Xuanhao.W.coding@outlook.com>
2022-05-08 11:37:55 +0800

    feat: add userList Pagination

--- app/apis/user_list/views.py
+++ app/apis/user_list/views.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.models import User
 from rest_framework.renderers import BrowsableAPIRenderer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
 from .serializer import UserListSerializer
 from ...renders import APIRenderer
+
+
+class UserListPagination(PageNumberPagination):
+    page_size_query_param = "pageSize"  # 每页最多数据条数
+    page_query_param = "pageIndex"  # 第几页
 
 
 class UserListView(ModelViewSet):
@@ -10,3 +16,6 @@ class UserListView(ModelViewSet):
     serializer_class = UserListSerializer
     lookup_field = 'username'
     renderer_classes = [APIRenderer, BrowsableAPIRenderer]
+    pagination_class = UserListPagination
+    def get_queryset(self):
+        return User.objects.all().order_by('id')
