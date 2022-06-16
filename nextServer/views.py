5980265d9a25fad123220c01d4970a94a3bc7e11
Pengyu Chen <95490459+spacecow-boy@users.noreply.github.com>
2022-06-16 11:15:08 +0800

    add token_authentication

--- nextServer/views.py
+++ nextServer/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from datetime import datetime
+
 
 def hello_world(request):
     context = {}
@@ -8,4 +13,10 @@ def hello_world(request):
 
 def hello(request):
     return HttpResponse("Welcome")
+
+@nextServer_view(['GET'])
+def index(request):
+    date = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    message = 'Clock In server is live cutrrent time is'
+    return Response(data=message+date,status=status.HTTP_200_OK)
 
