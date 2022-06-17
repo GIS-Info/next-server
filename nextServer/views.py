b251edf75449b971beb2bd2c77fda98b787d35a1
Pengyu Chen <95490459+spacecow-boy@users.noreply.github.com>
2022-06-17 20:21:40 +0800

    add restframework/checkserver

--- nextServer/views.py
+++ nextServer/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from datetime import datetime
 
 def hello_world(request):
     context = {}
@@ -9,3 +13,9 @@ def hello_world(request):
 def hello(request):
     return HttpResponse("Welcome")
 
+@api_view(['GET'])
+
+def index(request):
+    date = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    message = 'Clock In server is live cutrrent time is'
+    return Response(data=message+date,status=status.HTTP_200_OK)
