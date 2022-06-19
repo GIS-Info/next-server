4c5965917196243700c19619d32358ba32a9fff6
Pengyu Chen <95490459+spacecow-boy@users.noreply.github.com>
2022-06-19 15:42:02 +0800

    Delete authapp directory

--- authapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path,include
-from authapp import views
-
-urlpatterns = [
-    path('',include('djoser.urls')),
-    path('',include('djoser.urls.authtoken')),
-    path('restricted/',views.restricted),
-]
