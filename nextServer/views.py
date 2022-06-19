be65a2ad1908b1ef50d606e05e502c89fb5d14e6
Pengyu Chen <95490459+spacecow-boy@users.noreply.github.com>
2022-06-19 16:30:31 +0800

    Update views.py

--- nextServer/views.py
+++ nextServer/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from datetime import datetime
 
 def hello_world(request):
     context = {}
@@ -12,10 +8,3 @@ def hello_world(request):
 
 def hello(request):
     return HttpResponse("Welcome")
-
-@api_view(['GET'])
-
-def index(request):
-    date = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-    message = 'Clock In server is live cutrrent time is'
-    return Response(data=message+date,status=status.HTTP_200_OK)
