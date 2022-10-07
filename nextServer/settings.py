5224a61fe2c3a54bde3be0834217eda859778dad
xuanh.w <xuanh.w@outlook.com>
2022-10-07 10:28:50 +0800

    feat: 添加file logger

--- nextServer/settings.py
+++ nextServer/settings.py
@@ -37,7 +37,6 @@ DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -84,7 +83,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'nextServer.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -93,15 +91,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': env('DB_HOST'),
         'NAME': env('DB_NAME'),
-        'PORT':env('DB_PORT'),
+        'PORT': env('DB_PORT'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
-        'OPTIONS':{
+        'OPTIONS': {
             "init_command": "SET foreign_key_checks = 0;"
         }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -121,7 +118,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -132,7 +128,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
@@ -151,9 +146,78 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'app.exceptions.custom_exception_handler',
-    'DEFAULT_AUTHENTICATION_CLASSES':[ 
-         # add authentication
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # add authentication
         'rest_framework.authentication.TokenAuthentication',
         'knox.auth.TokenAuthentication',
     ],
+}
+
+LOGGING_DIR = BASE_DIR.joinpath("logs")
+debug_log_dir = LOGGING_DIR / "debug"
+info_log_dir = LOGGING_DIR / "info"
+error_log_dir = LOGGING_DIR / "error"
+debug_log_dir.mkdir(exist_ok=True, parents=True)
+info_log_dir.mkdir(exist_ok=True, parents=True)
+error_log_dir.mkdir(exist_ok=True, parents=True)
+# Logging
+LOGGING = {
+    'version': 1,  # 定义版本 1
+    'disable_existing_loggers': False,  # 允许使用已有的默认过滤器
+    'formatters': {  # 日志格式器
+        'verbose': {  # 定义一个格式器 verbose
+            'format': '[%(asctime)s] [%(levelname)s]\t[%(module)s]:\t%(message)s'
+            # 输出日志级别名称，日志消息以及生成日志消息的时间，进程，线程和模块
+        },
+        'simple': {  # 定义一个格式器 simple
+            'format': '[%(asctime)s] [%(levelname)s]:\t%(message)s'  # 仅输出日志级别名称（例如 DEBUG）和日志消息
+        },
+    },
+    'filters': {  # 日志过滤器
+        # 仅在DEBUG模式启用
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {  # 日志处理器
+        'console': {  # 定义一个处理器 console，将 INFO 级别的日志使用 stream 流处理打印到控制台
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'  # 过滤规则使用 simple，只输出日志等级以及 messages 信息
+        },
+        'file_info': {  # 定义一个处理器 file
+            'level': 'INFO',  # 定义 handler 的日志级别
+            'class': 'logging.handlers.TimedRotatingFileHandler',  # 使用文件类处理器，可以将日志写入文件中
+            'filename': LOGGING_DIR / 'info' / 'log',  # 定义日志信息的存储路径，文件路径需要确认有可写权限
+            'formatter': 'verbose',
+            'when': 'midnight',
+            'interval': 3,
+            'backupCount': 100,
+        },
+        'file_debug': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': LOGGING_DIR / 'debug' / 'log',
+            'formatter': 'verbose',
+            'when': 'midnight',
+            'interval': 1,
+            'backupCount': 100,
+        },
+        'file_error': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': LOGGING_DIR / 'error' / 'log',
+            'formatter': 'verbose',
+            'when': 'midnight',
+            'interval': 15,
+            'backupCount': 100,
+        },
+    },
+    'loggers': {  # 日志记录器
+        'django': {  # 定义一个记录器 django
+            'handlers': ['console', 'file_info', 'file_debug', 'file_error'],
+            'propagate': True,  # 允许传播至上级记录器
+        },
+    }
 }
