389842e50d531bca7fc5869562c3222bb600b170
Xuanh.W <Xuanhao.W.coding@outlook.com>
2022-10-23 01:15:06 +0800

    fix: swagger仅在debug模式下可用

--- nextServer/settings.py
+++ nextServer/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
     'accounts',
     'knox',
 ]
+if DEBUG:
+    INSTALLED_APPS += ['drf_yasg']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
