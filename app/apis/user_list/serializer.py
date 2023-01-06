1e3c94628dfce8ea1a1206476ce520911a42b4e9
xuanh.w <xuanh.w@outlook.com>
2023-01-06 18:52:53 +0800

    Revert "Revert "break: 用户模块添加权限验证，修改接口返回""
    This reverts commit c596c68b533c243affb8a3c682cdc9a960e3db7f.

--- app/apis/user_list/serializer.py
+++ app/apis/user_list/serializer.py
@@ -11,7 +11,7 @@ class UserListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ['username', 'password', 'email']
 
     def create(self, validated_data):
         validated_data['password'] = make_password(validated_data.get('password'))
